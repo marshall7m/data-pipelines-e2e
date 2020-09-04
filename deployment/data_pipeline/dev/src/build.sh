5f24583ab025bff90ddfce1f5de6e79c4eff7506
marshall7m <marshallmamiya@gmail.com>
2020-09-04 09:46:51 -1000

    fix silly docker build syntax error

--- deployment/data_pipeline/dev/src/build.sh
+++ deployment/data_pipeline/dev/src/build.sh
@@ -1,6 +1,6 @@
 docker build . \
     --tag $ECR_REPO_URL:$IMAGE_TAG \
-    --build-arg AIRFLOW_IMG_TAG="1.10.11"
+    --build-arg AIRFLOW_IMG_TAG="1.10.11" \
     --build-arg DAGS_FOLDER="dags/" \
     --build-arg PLUGINS_FOLDER="plugins/" \
     --build-arg CONFIG_FOLDER="cfg/" \
