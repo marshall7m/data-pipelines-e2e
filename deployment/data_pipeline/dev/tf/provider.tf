08e3fefb75c5fb375f6955e4c01561c26f7569d9
marshall7m <marshallmamiya@gmail.com>
2020-09-04 09:37:31 -1000

    add explicit build path/file for docker build CI, update aws provider version

--- deployment/data_pipeline/dev/tf/provider.tf
+++ deployment/data_pipeline/dev/tf/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY_ID
-  region = "us-west-2"
-  version = "~> 3.4"
+  region = var.AWS_DEFAULT_REGION
+  version = "~> 3.2.0"
 }
