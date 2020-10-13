f03bc99f0a5604ee0d215ac0d6ea656d0f47c6be
marshall7m <marshallmamiya@gmail.com>
2020-10-13 09:29:13 -0700

    add dag tf aws resource dir

---
+++ deployment/data_pipeline/dev/tf/sparkify_analytics/athena_queries.tf
@@ -0,0 +1,114 @@
+resource "aws_athena_database" "this" {
+  name   = "${replace(local.resource_prefix, "-", "_")}_db"
+  bucket = var.private_bucket
+}
+
+resource "aws_athena_workgroup" "this" {
+  name = "${local.resource_prefix}-athena-workgroup"
+
+  configuration {
+    enforce_workgroup_configuration =  true
+
+    result_configuration {
+      output_location = "s3://${var.private_bucket}/data_pipeline/${var.env}/athena_queries/"
+    }
+  }
+
+  tags = {
+      "environment" = var.env 
+      "project_id" = var.project_id
+      "client" = var.client
+      "service" = "data-pipeline"
+      "dag_id" = "sparkify-analytics"
+  }
+}
+
+resource "aws_athena_named_query" "top_day_songs" {
+  name = "${local.resource_prefix}-name"
+  database = aws_glue_catalog_database.this.id
+  workgroup = aws_athena_workgroup.this.name
+  query = <<EOF
+SELECT 
+    song,
+    artist,
+    COUNT(*)
+FROM songplay
+JOIN songs
+ON songs.artist_id = songplay.artist_id
+GROUP BY artist, song, COUNT(*)
+ORDER BY COUNT(*) DESC
+WHERE year = {}
+AND month = {}
+AND day = {}
+LIMIT 10
+EOF
+}
+
+resource "aws_athena_named_query" "top_month_songs" {
+  name = "${local.resource_prefix}-name"
+  database = aws_glue_catalog_database.this.id
+  workgroup = aws_athena_workgroup.this.name
+  query = <<EOF
+SELECT 
+    song,
+    artist,
+    COUNT(*)
+FROM songplay
+JOIN songs
+ON songs.artist_id = songplay.artist_id
+GROUP BY artist, song, COUNT(*)
+ORDER BY COUNT(*) DESC
+WHERE year = {}
+AND month = {}
+LIMIT 10
+EOF
+}
+
+resource "aws_athena_named_query" "top_year_songs" {
+  name = "${local.resource_prefix}-name"
+  database = aws_glue_catalog_database.this.id
+  workgroup = aws_athena_workgroup.this.name
+  query = <<EOF
+SELECT 
+    song,
+    artist,
+    COUNT(*)
+FROM songplay
+JOIN songs
+ON songs.artist_id = songplay.artist_id
+GROUP BY artist, song, COUNT(*)
+ORDER BY COUNT(*) DESC
+WHERE year = {}
+LIMIT 10
+EOF
+}
+
+resource "aws_athena_named_query" "avg_session_id_item" {
+  name = "${local.resource_prefix}-avg-session-id-item"
+  database = aws_glue_catalog_database.this.id
+  workgroup = aws_athena_workgroup.this.name
+  query = <<EOF
+SELECT
+    page,
+    ROUND(AVG(iteminsession), 1) AS avg_item_in_session,
+    ROUND(AVG(sessionid), 1) AS avg_session_id
+FROM staging_events
+WHERE 
+    page IN ('Upgrade', 'Downgrade', 'Submit Upgrade', 'Submit Downgrade')
+GROUP BY page
+EOF
+}
+
+resource "aws_athena_named_query" "upgrade_downgrade_count" {
+  name = "${local.resource_prefix}-upgrade_downgrade_count"
+  database = aws_glue_catalog_database.this.id
+  workgroup = aws_athena_workgroup.this.name
+  query = <<EOF
+SELECT 
+    page,
+    COUNT(*)
+FROM sparkify.staging_events
+GROUP BY page
+WHERE page IN ('Downgrade', 'Upgrade')
+EOF
+}
---
+++ deployment/data_pipeline/dev/tf/sparkify_analytics/glue_crawlers.tf
@@ -0,0 +1,108 @@
+resource "aws_iam_role" "glue" {
+    path = "/"
+    name = "${local.resource_prefix}-glue-crawler"
+    description = "Allows all glue crawlers to read access and all glue jobs read/write access to the appropriate S3 resources."
+    assume_role_policy = <<-EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "glue.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_role_policy" "glue" {
+  role = aws_iam_role.glue.name
+  name = "${local.resource_prefix}-glue-policy"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [   
+    {
+      "Effect": "Allow",
+      "Action": [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+      ],
+      "Resource": "${data.terraform_remote_state.CI_CD.outputs.private_bucket_arn}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "s3:GetObject"
+      ],
+      "Resource": "${data.terraform_remote_state.CI_CD.outputs.private_bucket_arn}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_glue_catalog_database" "this" {
+  name = "${local.resource_prefix}-glue-catalog-db"
+}
+
+resource "aws_glue_crawler" "raw" {
+  database_name = aws_glue_catalog_database.this.name
+  name          = "${local.resource_prefix}-glue-crawler-raw"
+  role          = aws_iam_role.glue.arn
+
+  s3_target {
+    path = "{s3://${data.terraform_remote_state.CI_CD.outputs.private_bucket_name}/data_pipeline/data/raw/year={year}/month={month}/day={day}/"
+  }
+
+  schema_change_policy {
+    update_behavior = "UPDATE_IN_DATABASE"
+    delete_behavior = "DEPRECATE_IN_DATABASE"
+  }
+
+  tags = {
+      "environment" = var.env 
+      "project_id" = var.project_id
+      "client" = var.client
+      "service" = "data-pipeline"
+      "task_id" = "crawl_raw_sources"
+      "dag_id" = "sparkify_analytics"
+  }
+}
+
+resource "aws_glue_crawler" "parquet" {
+  database_name = aws_glue_catalog_database.this.name
+  name          = "${local.resource_prefix}-glue-crawler-parquet"
+  role          = aws_iam_role.glue.arn
+
+  s3_target {
+    path = "s3://${data.terraform_remote_state.CI_CD.outputs.private_bucket_name}/data_pipeline/data/parquet/year={year}/month={month}/day={day}/"
+  }
+
+  schema_change_policy {
+        update_behavior = "UPDATE_IN_DATABASE"
+        delete_behavior = "DEPRECATE_IN_DATABASE"
+    }
+
+  tags = {
+      "environment" = var.env 
+      "project_id" = var.project_id
+      "client" = var.client
+      "service" = "data-pipeline"
+      "task_id" = "crawl_parquet_sources"
+      "dag_id" = "sparkify_analytics"
+  }
+}
